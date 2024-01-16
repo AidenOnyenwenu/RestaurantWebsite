c0cdc6f75d65b6402b6cbc4f98decd5517419c94
AidenistooOP <aidenonyenwenu@gmail.com>
2024-01-16 14:55:41 +0000

    This function is used for the hamburger menu navigation overlay.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 document.cookie = "Informatie=Cookie";
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.querySelector(".hamburger-menu").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0";
+    document.querySelector(".hamburger-menu").style.display = "block";
+}
